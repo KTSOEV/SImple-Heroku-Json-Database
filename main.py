d9da90b5479d1bd30fd63e15fa3bf3a2fd1bf409
Oleg Ktsoev <olegmernin@gmail.com>
2022-05-24 22:23:09 +0300

    make it better

---
+++ main.py
@@ -0,0 +1,21 @@
+from pysondb import db
+from flask import request, Flask, jsonify
+app = Flask(__name__)
+a = db.getDb("db")
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify(a.getAll())
+
+@app.route('/', methods=['PUT'])
+def put():
+    a.deleteAll()
+    a.addMany(request.json)
+    return jsonify(a.getAll())
+
+@app.route('/', methods=['DELETE'])
+def delete():
+    a.deleteAll()
+    return jsonify(a.getAll())
+
+app.run(debug=False)
